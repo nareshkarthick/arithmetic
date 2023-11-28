aa15f95c6406ec23401e20348f2194a77ca3dbeb
nareshkarthick <sn067027@gmail.com>
2023-11-28 11:05:35 +0530

    1st commit

---
+++ arithmetic.py
@@ -0,0 +1,11 @@
+num1 = int(input("enter the number:"))
+num2 = int(input("enter another number:"))
+
+#addition of two number
+sum=num1+num2
+
+#subtraction of two number
+sub=num1-num2
+
+print("the addition of two number is:",sum)
+print("the subtraction of two numbers:",sub)
