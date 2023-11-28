58f9ae2ff794eab43d20ab1ded705beefba25330
nareshkarthick <sn067027@gmail.com>
2023-11-28 11:13:27 +0530

    2nd commit

--- arithmetic.py
+++ arithmetic.py
@@ -7,5 +7,12 @@ sum=num1+num2
 #subtraction of two number
 sub=num1-num2
 
+#multiplication of two number
+mul=num1*num2
+
+#division of two number
+div=num1/num2
 print("the addition of two number is:",sum)
 print("the subtraction of two numbers:",sub)
+print("the multiplication of two number is:",mul)
+print("the division of two numbers:",div)
